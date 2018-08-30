87862130aec649bbbf38e81acc0a41d1a64a61f5
tprax <tprax01@gmail.com>
2018-08-30 14:11:46 -0600

    updated controllers and views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @pages = Page.all
+    @pages = Page.order(created_at: :asc)
   end
 
   def show
@@ -19,6 +19,25 @@ class PagesController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+      redirect_to page_path(@page.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Page.find(params[:id]).destroy
+    redirect_to pages_path
   end
 
   private

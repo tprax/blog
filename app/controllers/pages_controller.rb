b70a75dcb5288154055ab7102bafb92ce3716eda
tprax <tprax01@gmail.com>
2018-08-30 12:21:45 -0600

    updae contoller and views

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,29 @@
+class PagesController < ApplicationController
+  def index
+    @pages = Page.all
+  end
+
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  def new
+    @page = Page.new
+  end
+
+  def create
+    @page = Page.new(page_params)
+
+    if @page.save
+      redirect_to page_path(@page.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def page_params
+      params.require(:page).permit(:title, :author, :body)
+    end
+end
